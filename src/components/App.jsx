969e1a32ba9116bcb89b15ed53b08a5191ba035d
Sergii <nezalonov.serhii@gmail.com>
2023-03-12 19:26:42 +0200

    add logick phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './ContactList/Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onSubmitForm = contacts => {
+    this.setState({ contacts: [contacts, ...this.state.contacts] });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  removeContact = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const normalizeFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+
+    return (
+      <section>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitForm={this.onSubmitForm} contacts={contacts} />
+        <h2>Contacts</h2>
+        <Filter filter={filter} changeFilter={this.changeFilter} />
+        <ContactList
+          contacts={filteredContacts}
+          removeContact={this.removeContact}
+        />
+      </section>
+    );
+  }
+}
